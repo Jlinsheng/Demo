1edb4afb554497e745a6181acaf4d06fcf3bc6ee
dh_jiaolinsheng <yushuoweiwu0>
2020-01-13 14:21:38 +0800

    r

---
+++ demo/src/main/java/com/gjdw/service/Tservice.java
@@ -0,0 +1,14 @@
+package com.gjdw.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public interface Tservice {
+
+    public void getDeleteByPerson();
+
+    public void getAddByProject(String projectName);
+
+    
+
+}
