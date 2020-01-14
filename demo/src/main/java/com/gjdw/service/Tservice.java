a31c258757bb797f4ed0feb2ce01b97e263ad7ab
dh_jiaolinsheng <yushuoweiwu0>
2020-01-14 14:35:06 +0800

    Revert "12341234"
    This reverts commit 99f537567cc23f36faa6eb4864521f02555745c4.

--- demo/src/main/java/com/gjdw/service/Tservice.java
+++ demo/src/main/java/com/gjdw/service/Tservice.java
@@ -1,50 +1,14 @@
 package com.gjdw.service;
 
-import com.gjdw.pojo.ScProject;
 import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
 
 @Service
 public interface Tservice {
 
-    /**
-     * 人员信息删除
-     */
-    public void getDeleteByPerson(String personId);
+    public void getDeleteByPerson();
 
-    /**
-     * 专利信息删除
-     */
-    public void getDeleteByPatent(String patentId);
+    public void getAddByProject(String projectName);
 
-    /**
-     * 软著信息删除
-     */
-    public void getDeleteBySoft(String softId);
-
-    /**
-     * 论文信息删除
-     */
-    public void getDeleteByPaper(String paperId);
-
-    /**
-     * 获奖信息删除
-     */
-    public void getDeleteByPrize(String prizeId);
-
-    /**
-     * 基本信息录入
-     */
-    public void getAddByProject(ScProject scProject, MultipartFile file);
-
-    /**
-     * 成果信息录入
-     */
-    public void getAddByFinancing();
-
-    /**
-     * 融资信息录入
-     */
-    public void getAddByAchievement();
+    
 
 }
